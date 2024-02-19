75cbc216dab78b1eb5f3666fa30753219f101be2
DAChristina <155730764+DAChristina@users.noreply.github.com>
2024-02-19 22:01:45 +0000

    Visualisation of the model
    Visualisation of the model, focused on infective (Iv/V) and positive ((Ev+Iv)/V) mosquitoes given various numbers of InfHuman (IH/H, OR proportion of infective human with microfilaremia).

---
+++ VectorDynamicsModel_visualisation.R
@@ -0,0 +1,49 @@
+library(tidyverse)
+
+# wd =  # Change the working directory to the location of the *.csv files if downloaded to PC
+# setwd(wd)
+
+An_g <- read.csv("Output_InfHuman_Angambiae.csv")
+An_a <- read.csv("Output_InfHuman_Anarabiensis.csv")
+
+dat <- left_join(An_g, An_a, by = "InfHuman_values", relationship = "many-to-many")
+# x = An. gambiae (red)
+# y = An. arabiensis (blue)
+
+# Given all values of InfHuman
+par(mfrow = c(1,2))
+plot(dat$InfHuman_values, dat$Prev_loop.x, # An. gambiae
+     xlab = "Proportion of LF in human population (with microfilaremia)",
+     ylab = "Proportion of infective mosquitoes",
+     main = "Proportion of infective mosquitoes",
+     xlim = c(0,1), ylim = c(0,.45), type = "l", col = "red")
+lines(dat$InfHuman_values, dat$Prev_loop.y, col = "blue") # An. arabiensis
+
+plot(dat$InfHuman_values, dat$Pos_loop.x, # An. gambiae
+     xlab = "Proportion of LF in human population (with microfilaremia)",
+     ylab = "Proportion of positive mosquitoes",
+     main = "Proportion of positive mosquitoes",
+     xlim = c(0,1), ylim = c(0,.45), type = "l", col = "red")
+lines(dat$InfHuman_values, dat$Pos_loop.y, col = "blue") # An. arabiensis
+par(mfrow = c(1,1))
+
+
+# ONLY InfHuman below WHO threshold (either it is .01 or .02)
+dat <- dat %>% 
+  filter(InfHuman_values <= .020)
+
+par(mfrow = c(1,2))
+plot(dat$InfHuman_values, dat$Prev_loop.x, # An. gambiae
+     xlab = "Proportion of LF in human population below WHO thresholds",
+     ylab = "Proportion of infective mosquitoes",
+     main = "Proportion of infective mosquitoes",
+     xlim = c(0,.021), ylim = c(0,.014), type = "l", col = "red")
+lines(dat$InfHuman_values, dat$Prev_loop.y, col = "blue") # An. arabiensis
+
+plot(dat$InfHuman_values, dat$Pos_loop.x, # An. gambiae
+     xlab = "Proportion of LF in human population below WHO thresholds",
+     ylab = "Proportion of positive mosquitoes",
+     main = "Proportion of positive mosquitoes",
+     xlim = c(0,.021), ylim = c(0,.014), type = "l", col = "red")
+lines(dat$InfHuman_values, dat$Pos_loop.y, col = "blue") # An. arabiensis
+par(mfrow = c(1,1))
